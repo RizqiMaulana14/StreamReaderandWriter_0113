fe952fc28e097f97533e7954b24988c109b2f597
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-06-10 17:28:17 +0700

    Pembuatan Program Utama

--- ioFile.cpp
+++ ioFile.cpp
@@ -3,3 +3,10 @@
 #include <string>
 using namespace std;
 
+int main(){
+    string baris;
+    //Membuka file dalam mode menulis
+    ofstream outfile;
+    //Menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+}
