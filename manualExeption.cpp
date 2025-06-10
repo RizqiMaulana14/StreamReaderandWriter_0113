e979cee8c05bb5bca0fff81ef171ef6225e256cb
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-06-10 18:49:56 +0700

    Penyelesaian Program Utama

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -4,12 +4,16 @@ using namespace std;
 int main(){
     try{
         cout << "Selamat Belajar di Prodi TI UMY" << endl;
-        throw 0.5; //Melemparkan sebuah integer maka,
+        throw 0.5; //Melemparkan sebuah integer maka
         cout << "Pernyataan tidak akan dieksekusi" << endl;        
     }
     catch(int a){
         //Blok ini akan dieksekusi
-        cout << "pengecualian akan dieksekusi" << end;
+        cout << "Pengecualian akan dieksekusi" << endl;
     }
-    
+    catch(...){
+        /*Jika selain integer, maka blok ini akan dieksekusi*/
+        cout << "Default pengecualian dieksekusi" << endl;
+    }
+    return 0;
 }
