509e5faaa09e9f741447bd84d1bc5a87a059e2b9
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-06-10 18:17:15 +0700

    Penutupan Untuk Menulis

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -13,4 +13,16 @@ int main(){
     //Menunjuk ke sebuah nama file
     outfile.open(NamaFile + ".txt", ios::out);
     cout << ">= Menulis File, \'q' untuk keluar" << endl;
+    //Unlimited loop untuk menulis
+    while(true){
+        cout << "- ";
+        //Mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //Loop akan berhentii jika anda memasukkan karakter q
+        if (baris == "q") break;
+        //Menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
+    //Selesai dalam menulis sekarang tutup filenya
+    outfile.close();
 }
