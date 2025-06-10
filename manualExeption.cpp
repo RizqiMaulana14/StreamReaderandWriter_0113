ae574cb6210a05aba565dc77fe373545f86ed168
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-06-10 18:45:55 +0700

    Pembuatan Try

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main(){
+    try{
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0.5; //Melemparkan sebuah integer maka,
+        cout << "Pernyataan tidak akan dieksekusi" << endl;        
+    }
+    catch(int a)
+    {
+        std::cerr << e.what() << '\n';
+    }
     
 }
