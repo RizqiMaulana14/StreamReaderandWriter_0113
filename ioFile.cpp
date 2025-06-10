37ea3dcad07d666db6acc9b0288af0034ba6e9ba
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-06-10 17:31:13 +0700

    Pembuatan Isi While

--- ioFile.cpp
+++ ioFile.cpp
@@ -11,6 +11,12 @@ int main(){
     outfile.open("contohfile.txt");
     //Unlimited loop untuk menulis 
     while(true){
-        
+        cout << "- ";
+        //Mendapatkan setiap karakter dalam satu barus
+        getline(cin, baris);
+        //Loop akan berhenti jika anda memasukkan karakter q
+        if (baris == "q") break;
+        //Menulis dan memasukkan nilai dari "Baris" ke dalam file
+        outfile << baris << endl;
     }
 }
